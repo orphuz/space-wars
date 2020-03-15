ec1fcb32cc7a083aa7a25d73a9553d61288e4bfa
Christian Kalinna <c.kalinna@gmail.com>
2020-03-15 19:18:07 +0100

    Implement event list and add first unit test

--- helpers/event_man.py
+++ helpers/event_man.py
@@ -5,6 +5,12 @@ class Event_man():
     """
     Lorem Ipsum
     """
+
     def __init__(self):
         """ Initiate timer values und frame_drop_counter """
-        return
+        self._timed_events = []
+
+    def add_timed_event(self, event_function, trigger_time):
+        """ Add a time based event provided as "event_function" that will be trigger when "event_time is reached """
+        self._timed_events.append({"event": event_function, "time": trigger_time})
+        logging.warning(f"New timed event added: {self._timed_events[-1]}")
